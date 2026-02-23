6c651c3b7e87499d6fe3309f8aeaf837158b2471
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-02-23 16:38:51 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+# 1
+def kvadrat(son):
+    return son * son
+
+
+print(kvadrat(3))
+
+2
+def kvadrat(son):
+    return son + son
+
+
+print(kvadrat(3))
+
+# 4
+def juft_toq(son):
+    if son % 2 == 0:
+        print("Juft son")
+    else:
+        print("Toq son")
+
+juft_toq(10)
+juft_toq(7)
+
+
+# 5
+def tub(son):
+    if son % 2 == 0:
+        print("Tub ")
+    else:
+        print("Tub mas ")
+
+tub(10)
+tub(7)
+
+# 6
+def katta(a, b, c):
+    katta = a
+
+print(katta(5, 12, 8))
